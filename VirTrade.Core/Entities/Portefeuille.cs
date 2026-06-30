68b9c04928675523e02a7498ec01e0d72566abef
AinaTino <andriamananatino@gmail.com>
2026-06-30 21:14:53 +0300

    core: Add entity and enum definitions for core trading features

---
+++ VirTrade.Core/Entities/Portefeuille.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VirTrade.Core.Entities;
+
+public class Portefeuille
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Column(TypeName = "decimal(18,4)")]
+    public decimal SoldeCash { get; set; } = 0;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    [Required]
+    public int UtilisateurId { get; set; }
+
+    public required Utilisateur Utilisateur { get; set; }
+
+    public ICollection<Position> Positions { get; set; } = new List<Position>();
+}
---
+++ VirTrade.Core/Enums/Role.cs
@@ -0,0 +1,7 @@
+﻿namespace VirTrade.Core.Enums;
+
+public enum Role
+{
+    Trader,
+    Admin
+}
