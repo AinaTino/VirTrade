73be85222c74b2cf59d9c575c9bdc510ab922180
AinaTino <andriamananatino@gmail.com>
2026-07-01 00:27:29 +0300

    core: Implement `MatchingEngine` with trade execution logic and persistence

---
+++ VirTrade.Core/Interfaces/IMatchingRepository.cs
@@ -0,0 +1,13 @@
+﻿using VirTrade.Core.Entities;
+
+namespace VirTrade.Core.Interfaces;
+
+public interface IMatchingRepository
+{
+    Task PersisterTradeAsync(
+        Trade trade,
+        Ordre bid,
+        Ordre ask,
+        int qteMatch,
+        decimal prixExecution);
+}
