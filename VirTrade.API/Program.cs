cf8dd9c90d323f2d100c8fd70a3947d162b39e7a
Rj00955 <rojotiana016@gmail.com>
2026-07-08 10:33:28 +0300

    fix: resolve JSON cycle error and track MarketSimulatorHostedService

--- VirTrade.API/Program.cs
+++ VirTrade.API/Program.cs
@@ -1,25 +1,30 @@
+using VirTrade.API;
 using VirTrade.Core.Interfaces;
 using VirTrade.Core.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddOpenApi();
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler =
+            System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
 
 // TEMPORAIRE - Membre 3 : à remplacer par le vrai StockRepository (Membre 2 / AppDbContext)
 builder.Services.AddSingleton<IStockRepository, FakeStockRepository>();
+builder.Services.AddSingleton<IMarketSimulator, MarketSimulator>();
+builder.Services.AddHostedService<MarketSimulatorHostedService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
-
 app.MapControllers();
 
 app.Run();
