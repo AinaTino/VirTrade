0cf450a2aebd593edbf0ee3b23732a55eddd1ad7
AinaTino <andriamananatino@gmail.com>
2026-07-10 12:58:40 +0300

    infra: Update dependency injection for `OrdersRepository`
    - Replaced `IOrderRepository` and `OrderRepository` with `IOrdersRepository` and `OrdersRepository` in service registrations.

--- VirTrade.API/Program.cs
+++ VirTrade.API/Program.cs
@@ -106,7 +106,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // TODO : décommenter une fois Membre 1 livré IOrderRepository + IMatchingRepository
-builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IOrdersRepository, OrdersRepository>();
 builder.Services.AddScoped<IMatchingRepository, MatchingRepository>();
 builder.Services.AddScoped<IOrderBookService, OrderBookService>();
 builder.Services.AddScoped<IMatchingEngine, MatchingEngine>();
